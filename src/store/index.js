79a75e502879371f7a6771573ab3b0b454adc190
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-04-08 17:15:40 -0500

    add redux devtools chrome extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { createStore } from 'redux';
-import rootReducer from './reducers';
-import initialState from './reducers/initialState';
+import rootReducer, { initialState } from './reducers';
 
-export default createStore(rootReducer, initialState);
+export default createStore(
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux-immutable';
 import test from './test';
 
+export { default as initialState } from './initialState';
 export default combineReducers({ test });
