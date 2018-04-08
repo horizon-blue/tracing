aa49a966e25456dbe4529e52f4d98397dc5b257a
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-04-08 00:49:28 -0500

    Switch to semantic ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Segment } from 'semantic-ui-react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,8 +9,50 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Segment inverted>
+            <Button inverted>Standard</Button>
+            <Button inverted color="red">
+              Red
+            </Button>
+            <Button inverted color="orange">
+              Orange
+            </Button>
+            <Button inverted color="yellow">
+              Yellow
+            </Button>
+            <Button inverted color="olive">
+              Olive
+            </Button>
+            <Button inverted color="green">
+              Green
+            </Button>
+            <Button inverted color="teal">
+              Teal
+            </Button>
+            <Button inverted color="blue">
+              Blue
+            </Button>
+            <Button inverted color="violet">
+              Violet
+            </Button>
+            <Button inverted color="purple">
+              Purple
+            </Button>
+            <Button inverted color="pink">
+              Pink
+            </Button>
+            <Button inverted color="brown">
+              Brown
+            </Button>
+            <Button inverted color="grey">
+              Grey
+            </Button>
+            <Button inverted color="black">
+              Black
+            </Button>
+          </Segment>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
