b82d09f07675c8b5b02e3a2e98dedfc3f0b17123
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-04-08 05:10:24 -0500

    Design 2 gear icons

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,12 +1,16 @@
 import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import gear1 from './assets/gear1.svg';
+import gear2 from './assets/gear2.svg';
 
 class Header extends PureComponent {
     render() {
-        return <div style={{ color: 'white' }}>{this.props.title}</div>;
+        return (
+            <div>
+                <img src={gear1} alt="logo" className="header-logo" />
+                <img src={gear2} alt="logo" className="header-logo" />
+            </div>
+        );
     }
 }
 
-const mapStateToProps = state => ({ title: state.get('test') });
-
-export default connect(mapStateToProps)(Header);
+export default Header;
