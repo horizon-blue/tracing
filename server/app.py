c53654a1160394a0dc4e5a0f3511ad8599d3123a
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-04-30 14:36:20 -0500

    Add ssl route

--- server/app.py
+++ server/app.py
@@ -8,7 +8,17 @@ app = Flask(__name__)
 CORS(app, resources=r'/')
 
 # Add graphql endpoint
-app.add_url_rule('/', view_func=GraphQL.as_view('graphql', schema=schema, graphiql=True))
+app.add_url_rule('/', view_func=GraphQL.as_view('graphql',
+                                                schema=schema, graphiql=True))
+
+# for ssl authentication
+
+
+@app.route('/.well-known/acme-challenge/<token_value>')
+def letsencrpyt(token_value):
+    with open('.well-known/acme-challenge/{}'.format(token_value)) as f:
+        answer = f.readline().strip()
+    return answer
 
 
 @app.teardown_appcontext
